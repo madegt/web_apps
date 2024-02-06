69d2567213ba5dfc8aeda491a7cbca1299436c80
Madeleine Garbin <madeleine.garbin@kellogg.northwestern.edu>
2024-02-06 17:25:39 +0000

    companies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "/tacos", :controller =>"tacos", :action=>"index"
   get "/dice", :controller =>"dice", :action=>"index"
+  get "/companies", :controller =>"companies", :action=>"index"
+  #resources ="companies"
+  #this is like get /companies and everything 
 end
