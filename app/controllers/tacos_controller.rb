31df2e5444a4660764ec65b36e90e3f1bca4e8d0
Madeleine Garbin <madeleine.garbin@kellogg.northwestern.edu>
2024-02-06 16:11:11 +0000

    withruby

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -2,7 +2,12 @@ class TacosController < ApplicationController
 
 #create a method = action
 def index
-  render :inline =>"<h1>hello world </h1> i love tacos </p>"
+  #render :inline =>"<h1>hello world </h1> i love tacos </p>"
+
+  #render a template from other rout (index.html.erb)
+
+  render :template=>"tacos/index"
+
 end
 
 end
