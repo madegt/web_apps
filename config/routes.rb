88be81656d4285d7ce96919cba14516d770d42db
Madeleine Garbin <madeleine.garbin@kellogg.northwestern.edu>
2024-02-06 17:35:57 +0000

    contacts

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get "/companies", :controller =>"companies", :action=>"index"
   #resources ="companies"
   #this is like get /companies and everything 
+  get "/contacts", :controller =>"contacts", :action=>"index"
+  
 end
